e318eda7705c28cd72d3e732ad7436ba265ffa4d
DonOmri <116734859+DonOmri@users.noreply.github.com>
2025-05-17 22:18:32 +0300

    some node and starting BFSG

--- Algorithms.py
+++ Algorithms.py
@@ -5,14 +5,39 @@ from typing import List, Tuple
 import heapdict
 
 
+class GraphNode:
+    def __init__(self, state_num, successors):
+        self.state_num = state_num
+        self.successors = successors
+
 
 class BFSGAgent():
     def __init__(self) -> None:
-        raise NotImplementedError
+        self.CLOSE = {}
+        self.OPEN = []
 
     def search(self, env: HaifaEnv) -> Tuple[List[int], float, int]:
-        raise NotImplementedError
-        
+        init_state = env.reset()
+        self.OPEN.append({"id": init_state,"actions": []})
+
+        # Run on OPEN
+        while self.OPEN:
+            curr = self.OPEN.pop()
+            self.CLOSE[curr] = []
+
+
+            # Search among all successors
+            for action, successor in env.succ(curr).items():
+                pass
+                # Check if child is goal
+
+
+                # Add child
+                #if succ in close
+                #self.OPEN.append((successor, curr['actions']+action))
+
+
+
 
 
 class UCSAgent():
